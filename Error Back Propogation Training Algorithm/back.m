ab7946ffc8d46701c8907c510022333bff5dc068
propsandparty <Manthan_personal@Manthans-MacBook-Pro-2.local>
2017-10-19 01:36:42 -0400

    Added pseudo code

--- Error Back Propogation Training Algorithm/back.m
+++ Error Back Propogation Training Algorithm/back.m
@@ -1,3 +1,38 @@
+%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%                PSEUDO CODE FOR BACK PROPOGRATION TRAINING ALGORITHM
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%initialize network weights (often small random values)
+%  do
+%     forEach training example named ex
+%        prediction = neural-net-output(network, ex)  // forward pass
+%        actual = teacher-output(ex)
+%        compute error (prediction - actual) at the output units
+%        compute {\displaystyle \Delta w_{h}} \Delta w_h for all weights from hidden layer to output layer  // backward pass
+%        compute {\displaystyle \Delta w_{i}} \Delta w_i for all weights from input layer to hidden layer   // backward pass continued
+%        update network weights // input layer not modified by error estimate
+%  until all examples classified correctly or another stopping criterion satisfied
+%  return the network
+%
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
+
+
+
+
+
+
 clear all;clc;trai_pairs=210;out_neurons=3;hid_neurons=6;in_nodes=3;
 eata=0.25;emax=0.001;q=1;e=0;lamda=1;  t=1;
 tic
